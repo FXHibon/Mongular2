c4a4de59e3b96e14c516f41390d964700c422804
FXHibon <hibonfx@gmail.com>
2015-11-08 23:56:14 +0100

    Adding external template

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 
 var PATHS = {
-    src: ['frontend/**/*.ts', '!frontend/bower_components/**/*.*']
+    src: ['frontend/**/*.ts'],
+    raw: ['frontend/**/*.*', '!frontend/**/*.ts']
 };
 
 gulp.task('clean', function (done) {
@@ -24,8 +25,14 @@ gulp.task('ts2js', function () {
     return tsResult.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('play', ['ts2js'], function () {
-    gulp.watch(PATHS.src, ['ts2js']);
+gulp.task('raw', function () {
+    gulp.src(PATHS.raw)
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('play', ['ts2js', 'raw'], function () {
+    gulp.watch(PATHS.src, ['ts2js', 'raw']);
+    gulp.watch(PATHS.raw, ['ts2js', 'raw']);
     require('./backend/app');
 });
 
